1ae3477617d77c8cc575255d127cde1f20c0f512
Leerena <leerena7@gmail.com>
2023-08-03 12:02:18 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
+import Header from './components/Header';
+import Main from './components/Main/';
 
 function App() {
-  return (
-      <header className="App-header">
-        
-        <p>Hello React!</p>
-          
-        
-      </header>
-  );
+    return (
+        <>
+            <Header className="App-header">
+                <p>Hello React!</p>
+            </Header>
+            <Main />
+        </>
+    );
 }
 
 export default App;
